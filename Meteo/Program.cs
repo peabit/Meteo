1464d36171347e5119b31f4cbdb3950476f4daf4
dmitry.zarechnev@gmail.com <dmitry.zarechnev@gmail.com>
2022-12-02 19:43:33 +0300

    Simplify structure

--- Meteo/Program.cs
+++ Meteo/Program.cs
@@ -1,16 +1,13 @@
 using Meteo.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
-app.UseCors(builder => builder.AllowAnyOrigin());
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
-app.UseRouting();
-app.MapControllers();
+
 app.MapHub<MeteoHub>("/meteo");
 
 app.Run();
